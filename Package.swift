702d4ec82f81eb200ff076ef83fa353969fc0980
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-03-23 15:20:06 +0800

    update macOS requirements

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Discord.swift",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
