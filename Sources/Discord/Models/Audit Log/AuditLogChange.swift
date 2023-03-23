f177191c66044418502a174a356cbec46b31cdf4
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-03-23 16:31:31 +0800

    update newValue and oldValue to optionals

--- Sources/Discord/Models/Audit Log/AuditLogChange.swift
+++ Sources/Discord/Models/Audit Log/AuditLogChange.swift
@@ -9,10 +9,10 @@ import Foundation
 
 public struct AuditLogChange: Codable {
     /// mixed (matches object field's type) New value of the key
-    public var newValue: Mixed
+    public var newValue: Mixed?
     
     /// mixed (matches object field's type) Old value of the key
-    public var oldValue: Mixed
+    public var oldValue: Mixed?
     
     /// Name of the changed entity, with a few exceptions
     public var key: String
