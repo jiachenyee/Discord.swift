4b6eb92784be8075252a3f6885f0c12965d668f4
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-03-24 00:19:51 +0800

    fix ISO8601Timestamp decoder

--- Sources/Discord/Models/Reference/ISO8601Timestamp.swift
+++ Sources/Discord/Models/Reference/ISO8601Timestamp.swift
@@ -16,14 +16,62 @@ public struct ISO8601Timestamp: Codable {
         try container.encode(date.ISO8601Format())
     }
     
-    enum CodingKeys: CodingKey {
-        case date
-    }
-    
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
-        let rawValue = try container.decode(String.self)
+        let rawValue = try Array(container.decode(String.self))
         
-        self.date = try Date(rawValue, strategy: .iso8601)
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = .init(identifier: "UTC")!
+        
+        let components: DateComponents
+        
+        if rawValue.count == 32 {
+            guard let year = Int(String(rawValue[0...3])),
+                  let month = Int(String(rawValue[5...6])),
+                  let day = Int(String(rawValue[8...9])),
+                  let hour = Int(String(rawValue[11...12])),
+                  let minute = Int(String(rawValue[14...15])),
+                  let second = Int(String(rawValue[17...18])),
+                  let microSecond = Int(String(rawValue[20...25])) else {
+                throw DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: String(rawValue)))
+            }
+            components = DateComponents(
+                calendar: calendar,
+                year: year,
+                month: month,
+                day: day,
+                hour: hour,
+                minute: minute,
+                second: second,
+                nanosecond: microSecond * 1_000
+            )
+        } else if rawValue.count == 25 {
+            guard let year = Int(String(rawValue[0...3])),
+                  let month = Int(String(rawValue[5...6])),
+                  let day = Int(String(rawValue[8...9])),
+                  let hour = Int(String(rawValue[11...12])),
+                  let minute = Int(String(rawValue[14...15])),
+                  let second = Int(String(rawValue[17...18]))
+            else {
+                throw DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: String(rawValue)))
+            }
+            components = DateComponents(
+                calendar: calendar,
+                year: year,
+                month: month,
+                day: day,
+                hour: hour,
+                minute: minute,
+                second: second
+            )
+        } else {
+            throw DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: String(rawValue)))
+        }
+        
+        guard let date = calendar.date(from: components) else {
+            throw DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: String(rawValue)))
+        }
+        
+        self.date = date
     }
 }
