c9370b6bece244e03000f9ddad453bac9db2c675
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-03-24 01:17:17 +0800

    update audit log with threads and users

--- Sources/Discord/Models/Audit Log/AuditLog.swift
+++ Sources/Discord/Models/Audit Log/AuditLog.swift
@@ -11,18 +11,24 @@ import Foundation
 public struct AuditLog: Codable {
     /// List of application commands referenced in the audit log
     public var application_commands: [Command]
+    
     /// List of audit log entries, sorted from most to least recent
     public var audit_log_entries: [AuditLogEntry]
+    
     /// List of auto moderation rules referenced in the audit log
     public var auto_moderation_rules: [AutoModerationRule]
+    #warning("Incomplete")
 //    /// array of guild scheduled event objects    List of guild scheduled events referenced in the audit log
 //    public var guild_scheduled_events
 //    /// array of partial integration objects    List of partial integration objects
 //    public var integrations
-//    /// array of thread-specific channel objects    List of threads referenced in the audit log*
-//    public var threads
-//    /// array of user objects    List of users referenced in the audit log
-//    public var users
+    
+    /// List of threads referenced in the audit log*
+    public var threads: [Channel]
+    
+    /// List of users referenced in the audit log
+    public var users: [User]
+    
 //    /// array of webhook objects    List of webhooks referenced in the audit log
 //    public var webhooks:
 }
