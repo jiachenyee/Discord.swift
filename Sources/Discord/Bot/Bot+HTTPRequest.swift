3ee6dd9e91b1f8209bf3618264def6804dfec0f9
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-03-25 17:20:14 +0800

    missed out adding ContentType in the last commit

--- Sources/Discord/Bot/Bot+HTTPRequest.swift
+++ Sources/Discord/Bot/Bot+HTTPRequest.swift
@@ -16,7 +16,8 @@ public extension Bot {
                                  method: method,
                                  headers: [
                                     .authorization("Bot \(token)"),
-                                    .userAgent("DiscordBot (https://github.com/jiachenyee/Discord.swift, 0)")
+                                    .userAgent("DiscordBot (https://github.com/jiachenyee/Discord.swift, 0)"),
+                                    .contentType("application/json")
                                  ])
         
         let value = try await request.serializingData().value
