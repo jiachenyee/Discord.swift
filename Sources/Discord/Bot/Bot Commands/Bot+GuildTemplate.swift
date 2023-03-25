f3d2fc9b1d99d10f7273347d09f1bb3b181dbba0
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-03-26 02:49:41 +0800

    set guild template

--- Sources/Discord/Bot/Bot Commands/Bot+GuildTemplate.swift
+++ Sources/Discord/Bot/Bot Commands/Bot+GuildTemplate.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 public extension Bot {
-    func getGuildTemplate(code templateCode: String) async throws -> Data {
-        try await sendRequest(
+    func getGuildTemplate(code templateCode: String) async throws -> GuildTemplate {
+        try await sendRequest(GuildTemplate.self,
                               endpoint: "guilds/templates/\(templateCode)")
     }
 }
