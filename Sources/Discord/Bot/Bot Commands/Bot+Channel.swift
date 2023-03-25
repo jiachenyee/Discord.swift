d959b8a8645483d1fec9d33989f2a30079269c1e
Du Yuhan <yuhan.du@gmail.com>
2023-03-25 20:13:44 +0800

    Add documentations for the first half of channel functions

--- Sources/Discord/Bot/Bot Commands/Bot+Channel.swift
+++ Sources/Discord/Bot/Bot Commands/Bot+Channel.swift
@@ -8,11 +8,22 @@
 import Foundation
 
 public extension Bot {
+    /// Get a channel by ID.
+    /// - Parameter channelId: The channel ID of the target channel.
+    /// - Returns: Returns a channel object. If the channel is a thread, a thread member object is included in the returned result.
+    /// > Discord Reference: https://discord.com/developers/docs/resources/channel#get-channel
     func getChannel(_ channelId: Snowflake) async throws -> Channel {
         try await sendRequest(Channel.self,
                               endpoint: "/channels/\(channelId)")
     }
     
+    /// Update a channel's settings.
+    /// - Parameters:
+    ///   - channelId: The channel ID of the target channel.
+    ///   - updatedProperties: The properties and specifications
+    /// - Returns: Returns a channel on success, and a 400 BAD REQUEST on invalid parameters.
+    /// - Note: This endpoint supports the `X-Audit-Log-Reason` header.
+    /// > Discord Reference: https://discord.com/developers/docs/resources/channel#modify-channel
     func modifyChannel(_ channelId: Snowflake,
                        updatedProperties: ChannelUpdateRequest) async throws -> Channel {
         try await sendRequest(Channel.self,
@@ -21,11 +32,31 @@ public extension Bot {
                               data: updatedProperties)
     }
     
+    /// Delete a channel, or close a private message.
+    ///
+    /// Requires the `MANAGE_CHANNELS` permission for the guild, or `MANAGE_THREADS` if the channel is a thread. Deleting a category does not delete its child channels; they will have their `parent_id` removed and a Channel Update Gateway event will fire for each of them.
+    ///
+    /// - Parameter channelId: The channel ID of the target channel.
+    /// - Warning: Deleting a guild channel cannot be undone. Use this with caution, as it is impossible to undo this action when performed on a guild channel. In contrast, when used with a private message, it is possible to undo the action by opening a private message with the recipient again.
+    /// - Note: For Community guilds, the Rules or Guidelines channel and the Community Updates channel cannot be deleted.
+    /// - Note: This endpoint supports the `X-Audit-Log-Reason` header.
+    /// > Discord Reference: https://discord.com/developers/docs/resources/channel#deleteclose-channel
     func deleteChannel(_ channelId: Snowflake) async throws {
         try await sendRequest(endpoint: "/channels/\(channelId)",
                               method: .delete)
     }
     
+    /// Retrieves the messages in a channel.
+    ///
+    /// If operating on a guild channel, this endpoint requires the current user to have the `VIEW_CHANNEL` permission. If the channel is a voice channel, they must also have the `CONNECT` permission.
+    ///
+    /// If the current user is missing the `READ_MESSAGE_HISTORY` permission in the channel, then no messages will be returned.
+    ///
+    /// - Parameters:
+    ///   - channelId: The channel ID of the target channel.
+    ///   - filters: The filters applied to the returned channel messages.
+    /// - Returns: Returns an array of message objects on success.
+    /// > Discord Reference: https://discord.com/developers/docs/resources/channel#get-channel-messages
     func getChannelMessages(_ channelId: Snowflake,
                             using filters: ChannelMessageFilter = .filters()) async throws -> [Message] {
         try await sendRequest([Message].self,
@@ -33,12 +64,32 @@ public extension Bot {
                               parameters: filters.toParameters())
     }
     
+    /// Retrieves a specific message in a channel.
+    ///
+    /// If operating on a guild channel, this endpoint requires the current user to have the `VIEW_CHANNEL` and `READ_MESSAGE_HISTORY` permissions. If the channel is a voice channel, they must also have the `CONNECT permission`.
+    ///
+    /// - Parameters:
+    ///   - channelId: The channel ID of the target channel.
+    ///   - messageId: The message ID of the target message.
+    /// - Returns: Returns a message object on success.
+    /// > Discord Reference: https://discord.com/developers/docs/resources/channel#get-channel-message
     func getChannelMessage(_ channelId: Snowflake,
                            messageId: Snowflake) async throws -> Message {
         try await sendRequest(Message.self,
                               endpoint: "/channels/\(channelId)/messages/\(messageId)")
     }
     
+    /// Sends a message to guild text or DM channel.
+    ///
+    /// To create a message as a reply to another message, apps can include a `message_reference` with a `message_id`. The `channel_id `and `guild_id `in the `message_reference` are optional, but will be validated if provided.
+    ///
+    /// - Parameters:
+    ///   - channelId: The channel ID of the target channel.
+    ///   - message: The content of the message sent.
+    /// - Returns: Returns a message object. Fires a Message Create Gateway event.
+    /// - Note: This function has limitations, such as certain permissions that the user needs to have under specific contexts. Visit Discord Reference (linked below) for a full list of limitations.
+    /// > Discord Reference: https://discord.com/developers/docs/resources/channel#create-message
+    /// > Tip:  See [message formatting](https://discord.com/developers/docs/reference#message-formatting) for more information on how to properly format messages.
     func sendMessage(in channelId: Snowflake,
                      message: MessageRequest) async throws -> Message {
         try await sendRequest(Message.self,
@@ -47,6 +98,14 @@ public extension Bot {
                               data: message)
     }
     
+    /// Crosspost a message in an Announcement Channel to following channels.
+    ///
+    /// - Parameters:
+    ///   - channelId: The channel ID of the target channel.
+    ///   - messageId: The message ID of the target message.
+    /// - Returns: Returns a message object. Fires a Message Update Gateway event.
+    /// - Important: This endpoint requires the `SEND_MESSAGES` permission, if the current user sent the message, or additionally the `MANAGE_MESSAGES` permission, for all other messages, to be present for the current user.
+    /// > Discord Reference: https://discord.com/developers/docs/resources/channel#crosspost-message
     func crosspostMessage(in channelId: Snowflake,
                           messageId: Snowflake) async throws -> Message {
         try await sendRequest(Message.self,
@@ -54,6 +113,15 @@ public extension Bot {
                               method: .post)
     }
     
+    /// Create a reaction for the message.
+    /// - Parameters:
+    ///   - channelId: The channel ID of the channel where the message is in.
+    ///   - messageId: The message ID of the message to add a reaction to.
+    ///   - reaction: The reaction emoji.
+    /// - Important: This endpoint requires the `READ_MESSAGE_HISTORY` permission to be present on the current user. Additionally, if nobody else has reacted to the message using this emoji, this endpoint requires the `ADD_REACTIONS` permission to be present on the current user.
+    /// - Warning: The emoji must be URL Encoded or the request will fail with `10014: Unknown Emoji`.
+    /// - Note: To use custom emoji, you must encode it in the format `name:id` with the emoji name and emoji ID.
+    /// > Discord Reference: https://discord.com/developers/docs/resources/channel#create-reaction
     func addReaction(inChannel channelId: Snowflake,
                      toMessage messageId: Snowflake,
                      reaction: ReactionEmoji) async throws {
@@ -61,6 +129,14 @@ public extension Bot {
                               method: .put)
     }
     
+    /// Delete a reaction the current user has made for the message.
+    /// - Parameters:
+    ///   - channelId: The channel ID of the target channel.
+    ///   - messageId: The message ID of the target message.
+    ///   - reaction: The reaction emoji to delete.
+    /// - Warning: The emoji must be URL Encoded or the request will fail with `10014: Unknown Emoji`.
+    /// - Note: To use custom emoji, you must encode it in the format `name:id` with the emoji name and emoji ID.
+    /// > Discord Reference: https://discord.com/developers/docs/resources/channel#delete-own-reaction
     func deleteReaction(inChannel channelId: Snowflake,
                         onMessage messageId: Snowflake,
                         reaction: ReactionEmoji) async throws {
@@ -68,6 +144,16 @@ public extension Bot {
                               method: .delete)
     }
     
+    /// Deletes the reaction of another user.
+    /// - Parameters:
+    ///   - channelId: The channel ID of the target channel.
+    ///   - messageId: The message ID of the target message.
+    ///   - user: The user ID of the target user.
+    ///   - reaction: The reaction emoji to delete.
+    /// - Important: This endpoint requires the `MANAGE_MESSAGES` permission to be present on the current user.
+    /// - Warning: The emoji must be URL Encoded or the request will fail with `10014: Unknown Emoji`.
+    /// - Note: To use custom emoji, you must encode it in the format `name:id` with the emoji name and emoji ID.
+    /// > Discord Reference: https://discord.com/developers/docs/resources/channel#delete-user-reaction
     func deleteUserReaction(inChannel channelId: Snowflake,
                             onMessage messageId: Snowflake,
                             user: Snowflake,
@@ -76,6 +162,16 @@ public extension Bot {
                               method: .delete)
     }
     
+    /// Get a list of users that reacted with this emoji.
+    /// - Parameters:
+    ///   - channelId: The channel ID of the target channel.
+    ///   - messageId: The message ID of the target message.
+    ///   - reaction: The reaction emoji to check.
+    ///   - query: The specifics of the query.
+    /// - Returns: Returns an array of user objects on success.
+    /// - Warning: The emoji must be URL Encoded or the request will fail with `10014: Unknown Emoji`.
+    /// - Note: To use custom emoji, you must encode it in the format `name:id` with the emoji name and emoji ID.
+    /// > Discord Reference: https://discord.com/developers/docs/resources/channel#get-reactions
     func getReactions(inChannel channelId: Snowflake,
                       onMessage messageId: Snowflake,
                       reaction: ReactionEmoji,
@@ -85,6 +181,12 @@ public extension Bot {
                               parameters: query.toParameters())
     }
     
+    /// Deletes all reactions on a message.
+    /// - Parameters:
+    ///   - channelId: The channel ID of the target channel.
+    ///   - messageId: The message ID of the target message.
+    /// - Important: This endpoint requires the `MANAGE_MESSAGES` permission to be present on the current user.
+    /// > Discord Reference: https://discord.com/developers/docs/resources/channel#delete-all-reactions
     func deleteAllReactions(inChannel channelId: Snowflake,
                             onMessage messageId: Snowflake) async throws {
         try await sendRequest(endpoint: "/channels/\(channelId)/messages/\(messageId)/reactions",
